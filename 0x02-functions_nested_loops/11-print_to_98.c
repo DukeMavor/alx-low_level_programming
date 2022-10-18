4428947d0c414d5b7e70bfa2cb0f94ac5826b296
DukeMavor <victor.awunudo@gmail.com>
2022-10-18 15:49:11 +0000

    Final Updates

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,6 +2,15 @@
 #include "6-abs.c"
 #include <stdio.h>
 
+/**
+ * print_to_98 - Prints all natural numbers
+ * from n to 98.
+ *
+ * @n: input number.
+ *
+ * Return: no return.
+ */
+
 void print_to_98(int n)
 {
 	int i;
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,36 +1,34 @@
-include "main.h"
+#include "main.h"
 
 /**
- * times_table - times table function
+ * times_table - Prints the 9 times table
  *
+ * Return: no return
  */
 void times_table(void)
 {
-	int i;
-	int j;
+	int a, b, op;
 
-	for (i = 0; i <= 9; i++)
+	for (a = 0; a <= 9; a++)
 	{
-		for (j = 0; j <= 9; j++)
-		{
-			int prod = j * i;
-			if (j == 0)
+
+		_putchar(48);
+		for (b = 1; b <= 9; b++)
+	{
+			op = a * b;
+			_putchar(44);
+			_putchar(32);
+			if (op <= 9)
 			{
-				_putchar('0');
-			} else if (prod <= 9)
+				_putchar(32);
+				_putchar(op + 48);
+																												}
+			else
 			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(prod + '0');
-			} else
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(prod / 10 + '0');
-				_putchar(prod % 10 + '0');
+				_putchar((op / 10) + 48);
+				_putchar((op % 10) + 48);
 			}
-		}
-		_putchar('\n');
 	}
+	_putchar('\n');
+}
 }
