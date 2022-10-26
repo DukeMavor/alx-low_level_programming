24863dd3adae1b2ffe9b51dc074461d28ce1d2ef
Victor Awunudo <113598114+DukeMavor@users.noreply.github.com>
2022-10-26 19:22:07 +0100

    Update 103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,47 +1,33 @@
 #include "main.h"
 
 /**
- * infinite_add - adds two numbers
- * @n1: first number
- * @n2: second number
- * @r: buffer for result
- * @size_r: buffer size
+ * infinite_add - function with 4 arguments
+ * @n1: char type pointer argument
+ * @n2: char type pointer argument
+ * @r: char type pointer argument
+ * @size_r: int type argument
  *
- * Return: address of r or 0
+ * Description: adds two numbers from string
+ * Return: sum of two integers
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i, j, k, l, m, n;
+	int count, count2;
 
-	for (i = 0; n1[i]; i++)
-		;
-	for (j = 0; n2[j]; j++)
-		;
-	if (i > size_r || j > size_r)
-		return (0);
-	m = 0;
-	for (i -= 1, j -= 1, k = 0; k < size_r - 1; i--, j--, k++)
+	while (n1[count] != '\0')
+		count++;
+	while (n2[count2] != '\0')
+		count2++;
+
+	*r = *(r + size_r);
+	while (n1[count] > 0 || n1[count2] > 0)
 	{
-		n = m;
-		if (i >= 0)
-			n += n1[i] - '0';
-		if (j >= 0)
-			n += n2[j] - '0';
-		if (i < 0 && j < 0 && n == 0)
-		{
-			break;
-		}
-		m = n / 10;
-		r[k] = n % 10 + '0';
-	}
-	r[k] = '\0';
-	if (i >= 0 || j >= 0 || m)
-		return (0);
-	for (k -= 1, l = 0; l < k; k--, l++)
-	{
-		m = r[k];
-		r[k] = r[l];
-		r[l] = m;
+		if (n1[count] + n2[count2] > 0)
+			*r = n1[count - 1] + n2[count2 - 1] + 1;
+		else
+			*r = n1[count] + n2[count2];
+		count--;
+		count2--;
 	}
 	return (r);
 }
