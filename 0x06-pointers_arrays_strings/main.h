010b31b6f4c5f351176236cafd29eb6d3f15b89d
Victor Awunudo <113598114+DukeMavor@users.noreply.github.com>
2022-10-26 20:29:14 +0100

    Update main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,97 +1,30 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
-void print_number(int n);
-
-char *rot13(char *s);
-
-char *leet(char *s);
-
-char *cap_string(char *s);
-
-char *string_toupper(char *s);
-
-void reverse_array(int *a, int n);
-
-int _strcmp(char *s1, char *s2);
-
-char *_strncpy(char *dest, char *src, int n);
-
-char *_strncat(char *dest, char *src, int n);
+int _putchar(char c);
 
 char *_strcat(char *dest, char *src);
 
-int _atoi(char *s);
+char *_strncat(char *dest, char *src, int n);
 
-char *_strcpy(char *dest, char *src);
+char *_strncpy(char *dest, char *src, int n);
 
-void print_array(int *a, int n);
+int _strcmp(char *s1, char *s2);
 
-void puts_half(char *str);
+void reverse_array(int *a, int n);
 
-void puts2(char *str);
+char *string_toupper(char *);
 
-void rev_string(char *s);
+char *cap_string(char *);
 
-void print_rev(char *s);
+char *leet(char *);
 
-void _puts(char *str);
-
-int _putchar(char c);
-
-void print_alphabet(void);
-
-void print_alphabet_x10(void);
-
-int _islower(int c);
-
-int _isalpha(int c);
-
-int print_sign(int n);
-
-int _abs(int);
-
-int print_last_digit(int);
-
-void jack_bauer(void);
-
-void times_table(void);
-
-int add(int, int);
-
-void print_to_98(int n);
-
-void print_times_table(int n);
-
-int _isupper(int c);
-
-int _isdigit(int c);
-
-int mul(int a, int b);
-
-void print_numbers(void);
-
-void print_most_numbers(void);
-
-void more_numbers(void);
-
-void print_line(int n);
-
-void print_diagonal(int n);
-
-void print_square(int size);
-
-void print_triangle(int size);
+char *rot13(char *);
 
 void print_number(int n);
-
-void reset_to_98(int *n);
-
-void swap_int(int *a, int *b);
-
-int _strlen(char *s);
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 
 void print_buffer(char *b, int size);
+
 #endif
