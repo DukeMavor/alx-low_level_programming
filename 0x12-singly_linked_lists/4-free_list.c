b9397b1636230a730fd52b637ac563d760d28d35
Victor Awunudo <113598114+DukeMavor@users.noreply.github.com>
2022-11-24 07:14:07 +0100

    Create 4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_list - free memory of all nodes of list pointed to by `head'
+ * @head: pointer to beginning of list
+ */
+void free_list(list_t *head)
+{
+	list_t *next;
+
+	while (head)
+	{
+		next = head->next;
+		free(head->str);
+		free(head);
+		head = next;
+	}
+}
