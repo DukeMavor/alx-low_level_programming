8a82f5474b54e6ee5472d58023f54316f3753bd1
Victor Awunudo <113598114+DukeMavor@users.noreply.github.com>
2022-12-01 13:39:41 +0100

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,25 +1,32 @@
-#include "main.h"
-
+#include "holberton.h"
 /**
- * binary_to_uint - convert string representation of binary number to uint
- * @b: string to convert
- *
- * Return: the converted number, or 0 if `b' is NULL or contains
- * symbols besides 0 or 1.
+ * binary_to_uint - Entry Point
+ * @b: const char
+ * Return: 0
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int n = 0;
+	unsigned int res = 0;
+	int base = 1, i = 0;
 
-	if (!b)
-		return (n);
-	while (*b)
+	if (b == NULL)
+		return (0);
+
+	while (b[i + 1])
 	{
-		if (*b != '0' && *b != '1')
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
-		n *= 2;
-		n += *b - '0';
-		++b;
+		i++;
 	}
-	return (n);
+
+	while (i >= 0)
+	{
+		res += ((b[i] - '0') * base);
+		base *= 2;
+		i--;
+	}
+
+
+	return (res);
+
 }
